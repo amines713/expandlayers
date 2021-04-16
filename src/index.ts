533d3c6e911968331f618a4f625e3d74245beae5
Aaron <aaron.mines@zohomail.com>
2021-04-16 16:07:23 -0600

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,51 @@
-import { clone } from 'timm'
-
-function expandLayers(initBoundaries, minHeight = 20) {
-	const boundaries = Array.from(initBoundaries)
-
-	let heightIsArray = false
-
-	// HIA
-	if (Array.isArray(minHeight)) {
-		if (boundaries.length === minHeight.length+1) {
-			heightIsArray = true
-		}
-		else {
-			minHeight = 20
-		}
-	}
-
-	let layers = []
-
-	boundaries.forEach((b, i) => {
-		const k = Number(i)
-		if (k < boundaries.length - 1) {
-			layers.push(boundaries[k+1] - boundaries[k])
-		}
-	})
-
-	//Find first skinny layer
-	let a = layers.findIndex((l, i) => {
-		// HIA
-		if (heightIsArray) {
-			return l < minHeight[i]
-		}
-		return l < minHeight
-	})
-
-	if (a < 0) {//All Good, returning!
-		return boundaries
-	}
+interface Best {
+	eD: number;
+	i?: number
+}
 
 
-	//set = actual numbers to expand
-	let set = [layers[a]]
 
-	//delta = extra space needed per number
-	let delta = []
 
-	//Look for adjacent skinny layers
-	let j = a+1
-	while (j < layers.length) {
-		// HIA
-		let currentHeight = heightIsArray ? minHeight[j] : minHeight
-		if (layers[j] <= currentHeight) {
-			set.push(layers[j])
-			j++
-		}
-		else break
-	}
 
-	//Find how much room is needed = N
-	let N
-
-	// HIA
-	if (heightIsArray) {
-		//Choose which of the minHeights to use for N
-		N = minHeight.slice(a, a+set.length).reduce((a, b) => a + b, 0)
-		set.forEach((s, i) => {
-			delta[i] = minHeight[a+i] - s
-			N -= s
-		})
-	}
-	else {
-		N = set.length * minHeight
-		set.forEach((s, i) => {
-			delta[i] = minHeight - s
-			N -= s
-		})
-	}
-
+const computeNewBoundaries = (firstLayer: number, layers: number[], boundaries: number[], delta: number[], set: number[], N: number) => {
 	//squares = each term to square and add up. Will add i to each on iteration. Non-end ones get doubled
 	//First element always starts as 0
-	let squares = Array(delta.length+1).fill(0)
+	let squares = Array(delta.length + 1).fill(0)
 
 	delta.forEach((d, k) => {
 		while (k < delta.length) {
-			squares[k+1] -= d
+			squares[k + 1] -= d
 			k++
 		}
 	})
 
-	//Go through each configuration. Save the best one
-	//let configurations = []
-	let bestC = {eD: Infinity}
-	let i = 0
-
-	//Look for first/last layers
-	if (a === 0) {//First Layer. Keep N=0, only move down
-		N = 0
+	let best: Best = {
+		eD: Infinity
 	}
 
-	if (a + set.length === layers.length) {//Last Layer. Only move up
+	let i = 0
+
+	if (firstLayer + set.length === layers.length) {//Last Layer. Only move up
 		i = N
+	}
+
+	if (i !== N && firstLayer === 0) {//First Layer. Keep N=0, only move down
+		N = 0
 	}
 
 	while (i <= N) {
 		let eD = 0
+
 		squares.forEach((s, k) => {
-			if (k === 0 || k === squares.length-1) {//First or last
-				eD += Math.pow(s+i, 2)
+			if (k === 0 || k === squares.length - 1) {//First or last
+				eD += Math.pow(s + i, 2)
 			}
 			else {//Middle
-				eD += 2 * ( Math.pow(s+i, 2) )
+				eD += 2 * ( Math.pow(s + i, 2) )
 			}
 		})
-		if (eD < bestC.eD) {
-			bestC = {
+		if (eD < best.eD) {
+			best = {
 				i: i,
 				eD: eD
 			}
@@ -121,15 +54,135 @@ function expandLayers(initBoundaries, minHeight = 20) {
 	}
 
 	//Apply the best configuration to the original set
-	const newB = clone(boundaries)
-	const newSquares = squares.map((s) => s += bestC.i)
+	const newBoundaries = [...boundaries]
+	const newSquares = squares.map(s => s += best.i)
 
 	for (let s of newSquares) {
-		newB[a] -= s
-		a++
+		newBoundaries[firstLayer] -= s
+		firstLayer++
 	}
 
-	return expandLayers(newB, minHeight)
+	return newBoundaries
+}
+
+
+
+
+
+const getInitialBoundaries = (initBoundaries: number[]): number[][] => {
+	const boundaries: number[] = [...initBoundaries]
+	let layers: number[] = []
+
+	boundaries.forEach((b, i) => {
+		if (i < boundaries.length - 1) {
+			layers.push(boundaries[i + 1] - boundaries[i])
+		}
+	})
+
+	return [boundaries, layers]
+}
+
+
+
+const expandStaticLayers = (initBoundaries: number[], minHeight: number): number[] => {
+	let [boundaries, layers] = getInitialBoundaries(initBoundaries)
+
+	//Find first skinny layer
+	let firstLayer = layers.findIndex((l: number) => l < minHeight)
+
+	if (firstLayer < 0) {//All Good, returning!
+		return boundaries
+	}
+
+	//set = actual numbers to expand
+	let set: number[] = [ layers[firstLayer] ]
+
+	//delta = extra space needed per number
+	let delta: number[] = []
+
+	let nextLayer = firstLayer + 1
+
+	while (nextLayer < layers.length) {
+		let currentHeight = minHeight
+
+		if (layers[nextLayer] <= currentHeight) {
+			set.push(layers[nextLayer])
+			nextLayer++
+		}
+		else {
+			break
+		}
+	}
+
+	//Find how much room is needed = N
+	let N: number = set.length * minHeight
+
+	set.forEach((s, i) => {
+		delta[i] = minHeight - s
+		N -= s
+	})
+
+	const newBoundaries = computeNewBoundaries(firstLayer, layers, boundaries, delta, set, N)
+
+	return expandStaticLayers(newBoundaries, minHeight)
+}
+
+const expandArrayLayers = (initBoundaries: number[], minHeight: number[]): number[] => {
+	let [boundaries, layers] = getInitialBoundaries(initBoundaries)
+
+	//Find first skinny layer
+	let firstLayer = layers.findIndex((l: number, i: number) => {
+		return l < minHeight[i]
+	})
+
+	if (firstLayer < 0) {//All Good, returning!
+		return boundaries
+	}
+
+	//set = actual numbers to expand
+	let set: number[] = [ layers[firstLayer] ]
+
+	//delta = extra space needed per number
+	let delta: number[] = []
+
+	let nextLayer = firstLayer + 1
+
+	while (nextLayer < layers.length) {
+		let currentHeight = minHeight[nextLayer]
+
+		if (layers[nextLayer] <= currentHeight) {
+			set.push(layers[nextLayer])
+			nextLayer++
+		}
+		else {
+			break
+		}
+	}
+
+	//Find how much room is needed = N
+	let N: number = minHeight.slice(firstLayer, firstLayer + set.length).reduce((a, b) => a + b, 0)
+
+	set.forEach((s, i) => {
+		delta[i] = minHeight[firstLayer + i] - s
+		N -= s
+	})
+
+	const newBoundaries = computeNewBoundaries(firstLayer, layers, boundaries, delta, set, N)
+
+	return expandArrayLayers(newBoundaries, minHeight)
+}
+
+
+
+
+
+const expandLayers = (initBoundaries: number[], minHeight: number | number[] = 20): number[] => {
+	if (minHeight instanceof Array) {
+		return expandArrayLayers(initBoundaries, minHeight)
+	}
+	else {
+		return expandStaticLayers(initBoundaries, minHeight)
+	}
 }
 
 export default expandLayers
---
+++ tests/expandlayers.test.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai'
+import expand from '../src/index'
+
+it('Two plus two', () => {
+	expect(2 + 2).to.equal(4)
+})
+
+it('Should be [100, 120, 140]', () => {
+  expect(expand([100, 120, 140])).to.eql([100, 120, 140])
+})
+
+it('Should be [100, 120, 140]', () => {
+  expect(expand([120, 130, 140])).to.eql([100, 120, 140])
+})
+
+it('Should be [100, 120, 140]', () => {
+  expect(expand([100, 130, 140])).to.eql([100, 120, 140])
+})
+
+it('Should be [120, 130, 140] (10)', () => {
+  expect(expand([120, 130, 140], 10)).to.eql([120, 130, 140])
+})
+
+it('Should be [120, 130, 140] (10)', () => {
+  expect(expand([125, 130, 140], 10)).to.eql([120, 130, 140])
+})
+
+it('Should be [120, 130, 140] (10)', () => {
+  expect(expand([130, 131, 140], 10)).to.eql([120, 130, 140])
+})
+
+it('Should be [10, 120, 130, 140] (10)', () => {
+  expect(expand([10, 120, 130, 140], 10)).to.eql([10, 120, 130, 140])
+})
+
+it('Should be [9, 19, 120, 130, 140] (10)', () => {
+  expect(expand([9, 11, 135, 138, 140], 10)).to.eql([9, 19, 120, 130, 140])
+})
+
+it('Should be [1, 35, 45, 120, 130, 140] (10)', () => {
+  expect(expand([1, 39, 41, 135, 138, 140], 10)).to.eql([1, 35, 45, 120, 130, 140])
+})
+
+it('Should be [10, 20, 35, 40]', () => {
+	const nums = [10, 20, 30, 40]
+	const heights = [10, 15, 5]
+	
+	expect(expand(nums, heights)).to.eql([10, 20, 35, 40])
+})
